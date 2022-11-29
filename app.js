ac9de597428b832cdb64f4a61f00bb6a38c27bb1
Pizarro Matias <pizarromat@gmail.com>
2022-11-28 23:44:20 -0300

    correcion

--- app.js
+++ app.js
@@ -67,13 +67,15 @@ class Contenedor {
                 const productos = await fs.promises.readFile(this.file, 'utf-8');
                 return JSON.parse(productos);
             }catch(err){
+                //en caso de un error, el mensaje de error includes "El fichero o directorio no existe" osea que no lo encuentra o no aparece, va a retornar un array vacio ya que si retorna un arrays vacio y va a escribir el producto.txt, va a crear el archivo y no se frena el codigo en caso de otro error.
                 if(err.message.includes('El fichero o directorio no existe')) return [];
                 console.log(`error: ${err}`);
             }
         }
+        
 
         save = async obj => {
-            let productos = this.getAll();
+            let productos = await this.getAll();
             try {
                 let newId;
                 productos.length === 0 ? newId = 1 : newId = productos[productos.length-1].id + 1;
@@ -89,7 +91,7 @@ class Contenedor {
         getById = async id => {
             let productos = await this.getAll();
             try {
-                const obj = productos.find(id => productos.id === id);
+                const obj = productos.find(producto => producto.id === id);
                 return obj ? obj : null;
             } catch(err){
                 console.log(`error: ${err}`);
@@ -99,7 +101,7 @@ class Contenedor {
         deleteById = async id => {
             let productos = await this.getAll();
             try {
-                productos = productos.filter(productos => productos.id != id);
+                productos = productos.filter(producto => producto.id != id);
                 await this.writeFile(productos);
             }catch(err){
                 console.log(`error: ${err}`);
